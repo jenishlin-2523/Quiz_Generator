eea87d321dee5100843197198cf86ef23ccf529c
mairu0087 <jenishlinb@gmail.com>
2026-02-13 01:47:28 +0530

    frontend fixed

--- frontend/src/pages/StaffDashboard.jsx
+++ frontend/src/pages/StaffDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
+// import { Link } from "react-router-dom";
 import "../styles/Dashboard.css";
 
 const COURSE_DATA = {
