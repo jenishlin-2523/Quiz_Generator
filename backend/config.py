b8d19f38b8b24317d601b77e1112f0ef7b8c0041
mairu0087 <jenishlinb@gmail.com>
2026-02-12 11:43:42 +0530

    Initial commit

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,4 @@
 JWT_SECRET_KEY = "super-secret-key"
-GROQ_API_KEY = ""
 GROQ_MODEL = "llama-3.1-8b-instant"
 MONGO_URI = "mongodb+srv://jenishlinb_db_user:I4WR1bvg9NY0Kyfe@cluster0.bvblj13.mongodb.net/?appName=Cluster0"
 DB_NAME = "quiz_app"
